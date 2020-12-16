d1de2d1a03c473668a2aa9e6fbbfaa4294edf457
Md Ashiqul Islam <shaondeveloper@gmail.com>
2020-12-16 17:51:14 +0600

    Compressing updated

--- propicker/src/main/java/com/shaon2016/propicker/util/FileUtil.kt
+++ propicker/src/main/java/com/shaon2016/propicker/util/FileUtil.kt
@@ -12,7 +12,6 @@ import android.graphics.*
 import android.media.ExifInterface
 import android.net.Uri
 import android.os.Environment
-import android.util.Log
 import android.webkit.MimeTypeMap
 import java.io.*
 import java.text.SimpleDateFormat
@@ -60,6 +59,10 @@ object FileUtil {
         return MimeTypeMap.getSingleton().getExtensionFromMimeType(fileType)
     }
 
+    suspend fun getFileExtension(fileName: String): String? {
+        return fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length)
+    }
+
     @Throws(IOException::class)
     suspend fun copy(source: InputStream, target: OutputStream) {
         val buf = ByteArray(8192)
@@ -76,7 +79,7 @@ object FileUtil {
         try {
             // Compress the bitmap and save in png format
             val stream: OutputStream = FileOutputStream(file)
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, stream)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, stream)
             stream.flush()
             stream.close()
         } catch (e: IOException) {
@@ -120,142 +123,102 @@ object FileUtil {
         if (file.exists()) file.delete()
     }
 
+
     /**
      * It compresses the image maintaining the ratio
      * */
-    suspend fun compressImage(
-        context: Context,
-        uri: Uri,
-        maxWidth: Float,
-        maxHeight: Float
-    ): File {
-//        val filePath: String = FileUriUtils.getRealPath(context, uri)!!
-//        var scaledBitmap: Bitmap? = null
-//        val options = BitmapFactory.Options()
-//
-////      by setting this field as true, the actual bitmap pixels are not loaded in the memory. Just the bounds are loaded. If
-////      you try the use the bitmap here, you will get null.
-//        options.inJustDecodeBounds = true
-//        var bmp = BitmapFactory.decodeFile(filePath, options)
-//        var actualHeight = options.outHeight
-//        var actualWidth = options.outWidth
-//
-//        var imgRatio = actualWidth / actualHeight.toFloat()
-//        val maxRatio = maxWidth / maxHeight
-//
-////      width and height values are set maintaining the aspect ratio of the image
-//        if (actualHeight > maxHeight || actualWidth > maxWidth) {
-//            when {
-//                imgRatio < maxRatio -> {
-//                    imgRatio = maxHeight / actualHeight
-//                    actualWidth = (imgRatio * actualWidth).toInt()
-//                    actualHeight = maxHeight.toInt()
-//                }
-//                imgRatio > maxRatio -> {
-//                    imgRatio = maxWidth / actualWidth
-//                    actualHeight = (imgRatio * actualHeight).toInt()
-//                    actualWidth = maxWidth.toInt()
-//                }
-//                else -> {
-//                    actualHeight = maxHeight.toInt()
-//                    actualWidth = maxWidth.toInt()
-//                }
-//            }
-//        }
-//
-////      setting inSampleSize value allows to load a scaled down version of the original image
-//        options.inSampleSize = calculateInSampleSize(options, actualWidth, actualHeight)
-//
-////      inJustDecodeBounds set to false to load the actual bitmap
-//        options.inJustDecodeBounds = false
-//
-//        options.inTempStorage = ByteArray(16 * 1024)
-//        try {
-////          load the bitmap from its path
-//            bmp = BitmapFactory.decodeFile(filePath, options)
-//        } catch (exception: OutOfMemoryError) {
-//            exception.printStackTrace()
-//        }
-//        try {
-//            scaledBitmap = Bitmap.createBitmap(actualWidth, actualHeight, Bitmap.Config.ARGB_8888)
-//        } catch (exception: OutOfMemoryError) {
-//            exception.printStackTrace()
-//        }
-//        val ratioX = actualWidth / options.outWidth.toFloat()
-//        val ratioY = actualHeight / options.outHeight.toFloat()
-//        val middleX = actualWidth / 2.0f
-//        val middleY = actualHeight / 2.0f
-//        val scaleMatrix = Matrix()
-//        scaleMatrix.setScale(ratioX, ratioY, middleX, middleY)
-//        val canvas = Canvas(scaledBitmap!!)
-//        canvas.setMatrix(scaleMatrix)
-//        canvas.drawBitmap(
-//            bmp,
-//            middleX - bmp.width / 2,
-//            middleY - bmp.height / 2,
-//            Paint(Paint.FILTER_BITMAP_FLAG)
-//        )
-//
-////      check the rotation of the image and display it properly
-//        val exif: ExifInterface
-//        try {
-//            exif = ExifInterface(filePath)
-//            val orientation: Int = exif.getAttributeInt(
-//                ExifInterface.TAG_ORIENTATION, 0
-//            )
-//            Log.d("EXIF", "Exif: $orientation")
-//            val matrix = Matrix()
-//            when (orientation) {
-//                ExifInterface.ORIENTATION_ROTATE_90 -> {
-//                    matrix.postRotate(90F)
-//                    Log.d("EXIF", "Exif: $orientation")
-//                }
-//                3 -> {
-//                    matrix.postRotate(180F)
-//                    Log.d("EXIF", "Exif: $orientation")
-//                }
-//                7 -> {
-//                    matrix.postRotate(270F)
-//                    Log.d("EXIF", "Exif: $orientation")
-//                }
-//                8 -> {
-//                    matrix.postRotate(270F)
-//                    Log.d("EXIF", "Exif: $orientation")
-//                }
-//            }
-//            scaledBitmap = Bitmap.createBitmap(
-//                scaledBitmap, 0, 0,
-//                scaledBitmap.width, scaledBitmap.height, matrix,
-//                true
-//            )
-//        } catch (e: IOException) {
-//            e.printStackTrace()
-//        }
-//        var out: FileOutputStream? = null
-//        val file = getImageOutputDirectory(context)
-//        try {
-//            out = FileOutputStream(file)
-//
-////          write the compressed bitmap at the destination specified by filename.
-//            scaledBitmap!!.compress(Bitmap.CompressFormat.PNG, 80, out)
-//        } catch (e: FileNotFoundException) {
-//            e.printStackTrace()
-//        }
-//        return file
+     suspend fun compressImage(
+         context: Context,
+         uri: Uri,
+         maxWidth: Float,
+         maxHeight: Float
+     ): File {
 
+         val filePath: String = FileUriUtils.getRealPath(context, uri)!!
+         var scaledBitmap: Bitmap? = null
+         val options = BitmapFactory.Options()
 
-        val savedImagePath: String = FileUriUtils.getRealPath(context, uri)!!
-        var bmp = BitmapFactory.decodeFile(savedImagePath)
-        bmp = Bitmap.createScaledBitmap(
-            bmp,
-            bmp.width / 4,
-            bmp.height / 4,
-            true
-        )
+ //      by setting this field as true, the actual bitmap pixels are not loaded in the memory. Just the bounds are loaded. If
+ //      you try the use the bitmap here, you will get null.
+         options.inJustDecodeBounds = true
+         var bmp = BitmapFactory.decodeFile(filePath, options)
+         var actualHeight = options.outHeight
+         var actualWidth = options.outWidth
 
-        bmp = checkRotation(savedImagePath, bmp, context)
-        return bitmapToFile(context, bmp)
-    }
+         var imgRatio = actualWidth / actualHeight.toFloat()
+         val maxRatio = maxWidth / maxHeight
+
+ //      width and height values are set maintaining the aspect ratio of the image
+         if (actualHeight > maxHeight || actualWidth > maxWidth) {
+             when {
+                 imgRatio < maxRatio -> {
+                     imgRatio = maxHeight / actualHeight
+                     actualWidth = (imgRatio * actualWidth).toInt()
+                     actualHeight = maxHeight.toInt()
+                 }
+                 imgRatio > maxRatio -> {
+                     imgRatio = maxWidth / actualWidth
+                     actualHeight = (imgRatio * actualHeight).toInt()
+                     actualWidth = maxWidth.toInt()
+                 }
+                 else -> {
+                     actualHeight = maxHeight.toInt()
+                     actualWidth = maxWidth.toInt()
+                 }
+             }
+         }
+
+ //      setting inSampleSize value allows to load a scaled down version of the original image
+         options.inSampleSize = calculateInSampleSize(options, actualWidth, actualHeight)
+
+ //      inJustDecodeBounds set to false to load the actual bitmap
+         options.inJustDecodeBounds = false
+
+         options.inTempStorage = ByteArray(16 * 1024)
+         try {
+ //          load the bitmap from its path
+             bmp = BitmapFactory.decodeFile(filePath, options)
+         } catch (exception: OutOfMemoryError) {
+             exception.printStackTrace()
+         }
+         try {
+             scaledBitmap = Bitmap.createBitmap(actualWidth, actualHeight, Bitmap.Config.ARGB_8888)
+         } catch (exception: OutOfMemoryError) {
+             exception.printStackTrace()
+         }
+         val ratioX = actualWidth / options.outWidth.toFloat()
+         val ratioY = actualHeight / options.outHeight.toFloat()
+         val middleX = actualWidth / 2.0f
+         val middleY = actualHeight / 2.0f
+         val scaleMatrix = Matrix()
+         scaleMatrix.setScale(ratioX, ratioY, middleX, middleY)
+         val canvas = Canvas(scaledBitmap!!)
+         canvas.setMatrix(scaleMatrix)
+         canvas.drawBitmap(
+             bmp,
+             middleX - bmp.width / 2,
+             middleY - bmp.height / 2,
+             Paint(Paint.FILTER_BITMAP_FLAG)
+         )
+
+ //      check the rotation of the image and display it properly
+         try {
+             scaledBitmap = checkRotation(filePath, scaledBitmap, context)
+         } catch (e: IOException) {
+             e.printStackTrace()
+         }
+         var out: FileOutputStream? = null
+         val file = getImageOutputDirectory(context)
+         try {
+             out = FileOutputStream(file)
+
+ //          write the compressed bitmap at the destination specified by filename.
+             scaledBitmap!!.compress(Bitmap.CompressFormat.PNG, 75, out)
+         } catch (e: FileNotFoundException) {
+             e.printStackTrace()
+         }
+         return file
+     }
 
     /*After getting image check the orientation of the image*/
     @Throws(IOException::class)
@@ -283,6 +246,9 @@ object FileUtil {
         return bitmap
     }
 
+    /**
+     * This extension fuction is used to flip the image for front end camera
+     * */
     private fun Bitmap.flip(horizontal: Boolean, vertical: Boolean): Bitmap {
         val matrix = Matrix()
         matrix.preScale((if (horizontal) -1 else 1).toFloat(), (if (vertical) -1 else 1).toFloat())
